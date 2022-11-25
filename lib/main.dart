0921b508cf10de37970bf07a371cf9574c3db7fa
violekdev <71582866+violekdev@users.noreply.github.com>
2022-11-25 15:18:37 +0530

    updated color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,71 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Flutter Modal Sample';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xff005b7f),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
+        body: const ModalBottomSheet(),
       ),
     );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({super.key});
+class ModalBottomSheet extends StatelessWidget {
+  const ModalBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
-        child: const Text('showModalBottomSheet'),
+        child: const Text('Open BottomSheet'),
         onPressed: () {
           showModalBottomSheet<void>(
+            backgroundColor: Colors.transparent,
+            // shape: RoundedRectangleBorder(
+            //   borderRadius: BorderRadius.circular(20.0),
+            // ),
             context: context,
             builder: (BuildContext context) {
               return Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0)),
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF3366FF),
+                        Color(0xFF00CCFF),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      // begin:  FractionalOffset(0.0, 0.0),
+                      // end:  FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
                 height: 200,
-                color: Colors.amber,
+                // color: const Color(0xff7f2400),
+                // color: const Color(0xff0092cc),
                 child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      const Text('Modal BottomSheet'),
+                      const Text(
+                        'Modal BottomSheet',
+                        style: TextStyle(color: Colors.white),
+                      ),
                       ElevatedButton(
                         child: const Text('Close BottomSheet'),
                         onPressed: () => Navigator.pop(context),
